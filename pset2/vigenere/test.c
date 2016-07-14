4f3c4c27fb0bba6c6a08d6f1d4e44c76f2999f86
darknawg63@gmail.com <darknawg63@gmail.com>
2016-07-14 15:49:32 +0200

    iteration 1: encode message by applying hash

--- pset2/vigenere/test.c
+++ pset2/vigenere/test.c
@@ -1,14 +1,18 @@
 // iteration 1: map key's ascii code to alphabetical index where index
 // is zero based
+// iteration 1: encode message by applying hash
 #include <stdio.h>
 
 int
 main (void)
 {
   const int BASE_LOWER = 97;
-  int ch = 'a';
+  int key = 'b';
+  int hash = key - BASE_LOWER;
+  char message = 'M';
 
-  printf("ascii code of 'a' is %i\n", ch);
-  printf("alphabetical index of 'a' is %i\n", ch - BASE_LOWER);
+  printf("ascii code of '%c' is %i\n", key, key);
+  printf("alphabetical index of 'a' is %i\n", key - BASE_LOWER);
+  printf("Message encrypted is %c\n", message + hash);
   return 0;
 }
