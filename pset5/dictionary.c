ccd6217238299987d0e634ae68d42144fda0c7e2
darknawg63@gmail.com <darknawg63@gmail.com>
2016-09-02 22:15:04 +0200

    pset5: implemented load function dictionary.c

---
+++ pset5/dictionary.c
@@ -0,0 +1,115 @@
+/**
+ * dictionary.c
+ *
+ * Computer Science 50
+ * Problem Set 5
+ *
+ * Implements a dictionary's functionality.
+ */
+
+#include <stdbool.h>
+
+#include "dictionary.h"
+
+#define APOSTROPHE -58
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char* word)
+{
+  // TODO
+  return false;
+}
+
+/**
+ * Loads dictionary into memory.  Returns true if successful else false.
+ */
+bool load(const char* dictionary)
+{
+  parent = newNode();
+  Node* cursor = parent;
+  Node* next;
+
+  char word[LENGTH];
+  FILE* fp;
+
+  if ((fp= fopen(dictionary, "r")) != NULL)
+    {
+      int i = 0;
+      int m; // Track the aphabetical index
+
+      while (fgets(word, sizeof(word), fp) != NULL)
+        {
+          while (word[i])
+            {
+              // We'll need to check if the element is in use. NOT NULL
+              m = hash(word[i]);
+              // If it's an apostrophe then kick it to index 27
+              if (m == APOSTROPHE)
+                m = 26;
+              if (cursor->child[m] == 0)
+                {
+                  next = newNode();
+                  cursor->child[m] = next;
+                  cursor = next;
+                }
+              else
+                {
+                  cursor = cursor->child[m];
+                  i++; // I had forgotten to increment lol...
+                  continue;
+                }
+              // printf("%d ", hash(word[i]));
+              if (word[i + 1] == '\n')
+                {
+                  word[i + 1] = '\0';
+                  strncpy(cursor->word, word, LENGTH);
+                  cursor = parent; //rewind back to the root
+                  i = 0;
+                  break;
+                }
+              i++;
+            }
+        }
+      return true;
+    }
+  // TODO
+  return false;
+}
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void)
+{
+  // TODO
+  return 0;
+}
+
+/**
+ * Unloads dictionary from memory.  Returns true if successful else false.
+ */
+bool unload(void)
+{
+  // TODO
+  return false;
+}
+
+int
+hash (char ch)
+{
+  int index;
+  index = ch - BASE;
+
+  return index;
+}
+
+Node*
+newNode ()
+{
+  Node* node = calloc(1, sizeof(Node));
+
+  //for (int i = 0; i < LENGTH; i++)
+  //  node->child[i] = NULL;
+  return node;
+}
---
+++ pset5/dictionary.h
@@ -0,0 +1,69 @@
+/**
+ * dictionary.h
+ *
+ * Computer Science 50
+ * Problem Set 5
+ *
+ * Declares a dictionary's functionality.
+ */
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+
+// maximum length for a word
+// (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
+#define LENGTH 45
+#define CAPACITY 27
+
+// ascii lower case starting index
+#define BASE 97
+
+
+/*
+ * Define our custom node for the trie
+ */
+typedef struct _node
+{
+  char word[LENGTH];
+  struct _node* child[CAPACITY];
+} Node;
+
+Node* parent;
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char* word);
+
+/**
+ * Loads dictionary into memory.  Returns true if successful else false.
+ */
+bool load(const char* dictionary);
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void);
+
+/**
+ * Unloads dictionary from memory.  Returns true if successful else false.
+ */
+bool unload(void);
+
+/**
+ * Maps the ascii index zero index
+ */
+int hash(char ch);
+
+
+/*
+ * Helper function for allocation of new nodes
+ */
+Node* newNode();
+#endif // DICTIONARY_H
